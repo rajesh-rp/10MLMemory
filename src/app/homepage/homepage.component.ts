9a7fb412de70926d1ba0efeb563ba03eabe7affa
rajesh-rp <pradhanrajesh845@gmail.com>
2021-05-10 16:54:37 +0530

    added carousel feature for mobile view for testimonials

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
+   
+  show=true;
+  screenWidth:any;
+  addClass=false;
   constructor() { }
-
+  
   ngOnInit(): void {
+    
   }
+
+  onResize(event:any){
+    this.screenWidth=event.target.innerWidth;
+    if(this.screenWidth<1302){
+      this.show=false;
+      this.addClass=true;
+    }
+    else{
+      this.show=true;
+      this.addClass=false;
+    }
+  }
+   
 
 }
